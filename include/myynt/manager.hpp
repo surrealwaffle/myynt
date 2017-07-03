b9f867ca85186745e741c6db376a04e9df08c108
surrealwaffle <aomaniac@gmail.com>
2017-07-03 15:52:59 -0700

    basic manager framework and implementation

--- include/myynt/manager.hpp
+++ include/myynt/manager.hpp
@@ -11,7 +11,7 @@
 #define MYYNT__MANAGER_HPP
 
 #include <tuple>        // std::tuple
-#include <utility>      // std::forward
+#include <utility>      // std::forward, std::index_sequence, std::make_index_sequence
 #include <type_traits>  // std::decay
 
 namespace myynt {
@@ -35,21 +35,41 @@ namespace myynt {
         
         /** \brief Sends \a message down to the submodules under the manager, by lvalue reference.
          *
-         * \return `std::forward<Message>(message)`
-         */
-        template< class Message >
-        constexpr typename std::decay<Message>::type
-        myynt_Process(Message&& message);
-        
-        /** \brief Sends \a message down to the submodules under the manager, by lvalue reference.
+         * If this function is passed an lvalue, then this function returns an lvalue reference.
+         * Otherwise, this function this function attempts to return \a message by move.
          *
          * \return `std::forward<Message>(message)`
          */
         template< class Message >
-        constexpr typename std::decay<Message>::type
-        myynt_Emit(Message&& message);
+        constexpr Message
+        myynt_Process(Message&& message) 
+        { return this->myynt_ProcessModulesByIndex(std::forward<Message>(message), std::make_index_sequence<sizeof...(Modules)>()); }
+        
+        /** \brief Sends \a message down to the submodules under the manager, by lvalue reference.
+         *
+         * If this function is passed an lvalue, then this function returns an lvalue reference.
+         * Otherwise, this function this function attempts to return \a message by move.
+         *
+         * \return `std::forward<Message>(message)`
+         */
+        template< class Message >
+        constexpr Message
+        myynt_Emit(Message&& message) 
+        { return this->myynt_Process(std::forward<Message>(message)); }
         
     private:
+        /** \brief Sends \a message down to the submodules selected by the supplied index sequence under the manager, by lvalue reference.
+         *
+         * If this function is passed an lvalue, then this function returns an lvalue reference.
+         * Otherwise, this function this function attempts to return \a message by move.
+         *
+         * \return `std::forward<Message>(message)`
+         */
+        template< class Message, std::size_t... I >
+        constexpr Message
+        myynt_Process(Message&& message, [[maybe_unused]] std::index_sequence<I...>);
+        
+        
         std::tuple<Modules...> modules_;
     };
     
@@ -57,6 +77,13 @@ namespace myynt {
     template< class... Modules >
     manager(Modules&&...) -> manager<typename std::decay<Modules>::type...>;
     #endif
+    
+    template< class Message, std::size_t... I >
+    constexpr Message
+    myynt_Process(Message&& message, [[maybe_unused]] std::index_sequence<I...>) {
+        (..., std::get<I>(modules_).myynt_Process(message));
+        return std::forward<Message>(message);
+    }
 }
 
 #endif // MYYNT__MANAGER_HPP
