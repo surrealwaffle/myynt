2b15fbfe9551eef28de12fa9b81ac2a527085d61
surrealwaffle <aomaniac@gmail.com>
2017-07-25 22:40:07 -0700

    basic traits for tags

---
+++ include/myynt/detail/tags.hpp
@@ -0,0 +1,59 @@
+//          Copyright Thomas Goehner 2017
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+/** \file tags.hpp
+ * \brief Contains tag categories and tag list retrieval.
+ */
+
+#ifndef MYYNT__TAGS_HPP
+#define MYYNT__TAGS_HPP
+
+#include <yymp/typelist_fwd.hpp>    // yymp::retain_as_typelist, yymp::is_not_empty
+#include <yymp/setops.hpp>          // yymp::intersect
+
+#include <type_traits>
+
+namespace myynt::tags {
+    
+    struct first;
+    
+    struct intermediate;
+    
+    struct last;
+    
+    template< class T, class Category, class = std::void_t<> >
+    struct tags_of_category {
+        using type = typename yymp::retain_as_typelist<typename T::myynt_tags>::type;
+    };
+    
+    template< class T >
+    struct tags_of_category<T, first, std::void_t<typename T::myynt_first>> {
+        using type = typename yymp::retain_as_typelist<typename T::myynt_first>::type;
+    };
+    
+    template< class T >
+    struct tags_of_category<T, intermediate, std::void_t<typename T::myynt_tags>> {
+        using type = typename yymp::retain_as_typelist<typename T::myynt_tags>::type;
+    };
+    
+    template< class T >
+    using tags_of = tags_of_category<T, intermediate>;
+    
+    template< class T >
+    struct tags_of_category<T, last, std::void_t<typename T::myynt_last>> {
+        using type = typename yymp::retain_as_typelist<typename T::myynt_last>::type;
+    };
+    
+    template< class S, class T, class Category >
+    struct belongs_to_category
+        : yymp::is_not_empty<
+            typename yymp::intersect<
+                typename yymp::tags_of<S>::type,
+                typename yymp::tags_of_category<T, Category>::type
+            >::type
+        > { };
+}
+
+#endif // MYYNT__TAGS_HPP
--- include/myynt/traits.hpp
+++ include/myynt/traits.hpp
@@ -11,7 +11,9 @@
 #define MYYNT__TRAITS_HPP
 
 #include <utility>		// std::declval
-#include <type_traits> 	// std::void_t, std::false_type, std::true_type
+#include <type_traits> 	// std::void_t, std::false_type, std::true_type, std::conditional
+
+#include <myynt/detail/tags.hpp>
 
 namespace myynt {
 	
@@ -25,6 +27,16 @@ namespace myynt {
     /** \brief Inherits from `bool` constants to indicate if a \a Module member function `myynt_RegisterManager` exists that takes values of type `Manager&&`. */
     template< class Manager, class Module, class = std::void_t<> >
     struct is_module_registerable;
+    
+    /** \brief Defines `type` as the tag category of \a Module under processing \a Message.
+     *
+     * `type` is one of:
+     *  * \ref myynt::tags::first
+     *  * \ref myynt::tags::intermediate
+     *  * \ref myynt::tags::last
+     */
+    template< class Module, class Message >
+    struct module_process_category;
     
     template< class Message, class Module, class >
     struct is_message_processable : std::false_type { };
@@ -45,6 +57,23 @@ namespace myynt {
         Module,
         std::void_t<decltype(std::declval<Module>().myynt_RegisterManager(std::declval<Manager>()))>
     > : std::true_type { };
+    
+    template< class Module, class Message >
+    struct module_process_category {
+        static constexpr bool is_in_first = tags::belongs_to_category<Module, Message, tags::first>::value;
+        static constexpr bool is_in_intermediate = tags::belongs_to_category<Module, Message, tags::intermediate>::value;
+        static constexpr bool is_in_last = tags::belongs_to_category<Module, Message, tags::last>::value;
+        
+        using type = typename std::conditional<
+            is_in_first,
+            tags::first,
+            typename std::conditional<
+                is_in_last,
+                tags::last,
+                tags::intermediate
+            >::type
+        >::type;
+    };
     
 }
 
