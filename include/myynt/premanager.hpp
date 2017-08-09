efe6f3c5b8eb3c3ddd929b1a634ec2d772be35f1
surrealwaffle <aomaniac@gmail.com>
2017-08-09 13:27:50 -0700

    myynt::premanager as an alias that completes types to myynt::impl::premanager

--- include/myynt/premanager.hpp
+++ include/myynt/premanager.hpp
@@ -29,172 +29,181 @@
 
 namespace myynt {
     
+    namespace impl {
+        template< class Manager, class... Modules >
+        struct premanager;
+    }
+    
     /** \brief Provides manager implementations the `myynt_Process` method, which emits a message down into its submodules. 
      *
      * This design makes use of CRTP in \a Manager.
+     * The implementation of the premanager functionality is left to \ref myynt::impl::premanager.
      */
     template< class Manager, class... Modules >
-    class premanager;
+    using premanager = impl::premanager<Manager, typename make_complete<Manager, Modules>::type...>;
 
-    template< class Manager, class... Modules >
-    class premanager {
-        using module_types = typename yymp::transform<
-            yymp::bind<make_complete<Manager, yymp::var>>::template generic,
-            yymp::typelist<Modules...>
-        >::type;
-        
-        using distinct_module_types = typename yymp::filter_duplicates<module_types>::type;
-        
-        using modules_container_type = typename yymp::expand_into<std::tuple, module_types>::type;
-        
-        /** \brief The modules themselves. */
-        modules_container_type modules_;
-        
-        template< class PossiblyCompleteRequest >
-        using complete_with_this_manager = typename make_complete<premanager, PossiblyCompleteRequest>::type;
-    public:
-        premanager()                    = delete; ///< Available only if each module is default constructible.
-        premanager(premanager const&)   = delete; ///< \todo make optionally available
-        premanager(premanager&&)        = delete; ///< \todo make optionally available
-        
-        premanager& operator=(premanager const&)    = delete; ///< \todo make optionally available
-        premanager& operator=(premanager&&)         = delete; ///< \todo make optionally available
-        
-        /** \brief Conditionally defined default constructor.
+    namespace impl {
+        /** \brief Implements premanager functionality.
          *
-         * This constructor is enabled if and only if all \a Modules are default constructible.
+         * No reinterpretation of \a Modules is performed.
          */
-        template<
-            bool B = std::is_default_constructible<decltype(modules_)>::value,
-            class = typename std::enable_if<B>::type
-        > explicit constexpr premanager() noexcept(std::is_nothrow_default_constructible<decltype(modules_)>::value)
-            : modules_() {
-            myynt_RegisterAsManager();
-        }
+        template< class Manager, class... Modules >
+        class premanager {
+            using module_types = typename yymp::typelist<Modules...>;
+            
+            using distinct_module_types = typename yymp::filter_duplicates<module_types>::type;
+            
+            using modules_container_type = typename yymp::expand_into<std::tuple, module_types>::type;
+            
+            /** \brief The modules themselves. */
+            modules_container_type modules_;
+            
+            template< class PossiblyCompleteRequest >
+            using complete_with_this_manager = typename make_complete<premanager, PossiblyCompleteRequest>::type;
+        public:
+            premanager()                    = delete; ///< Available only if each module is default constructible.
+            premanager(premanager const&)   = delete; ///< \todo make optionally available
+            premanager(premanager&&)        = delete; ///< \todo make optionally available
+            
+            premanager& operator=(premanager const&)    = delete; ///< \todo make optionally available
+            premanager& operator=(premanager&&)         = delete; ///< \todo make optionally available
+            
+            /** \brief Conditionally defined default constructor.
+             *
+             * This constructor is enabled if and only if all \a Modules are default constructible.
+             */
+            template<
+                bool B = std::is_default_constructible<decltype(modules_)>::value,
+                class = typename std::enable_if<B>::type
+            > explicit constexpr premanager() noexcept(std::is_nothrow_default_constructible<decltype(modules_)>::value)
+                : modules_() {
+                myynt_RegisterAsManager();
+            }
+            
+            /** \brief Constructs the premanager's modules from \a modules. 
+             *
+             * This constructor is enabled if and only if all of the following are true:
+             *  * `sizeof...(UTypes) >= 1`
+             *  * `sizeof...(UTypes) == sizeof...(Modules)`
+             *  * `std::is_constructible<Modules, U&&>` is \c true for each type `U` in the pack `UTypes`
+             */
+            template<
+                class... UTypes, 
+                class = typename std::enable_if<
+                    sizeof...(UTypes) >= 1 &&
+                    sizeof...(UTypes) == sizeof...(Modules) &&
+                    (std::is_constructible<Modules, UTypes&&>::value && ...)    
+                >::type
+            > explicit constexpr premanager(UTypes&&... modules) : modules_(std::forward<UTypes>(modules)...) {
+                myynt_RegisterAsManager();
+            }
+            
+            /** \brief Sends \a message down to the submodules under the premanager, by lvalue reference.
+             *
+             * If this function is passed an lvalue, then this function returns an lvalue reference.
+             * Otherwise, this function this function attempts to return \a message by move.
+             *
+             * \return `std::forward<Message>(message)`
+             */
+            template< class Message >
+            constexpr Message
+            myynt_Process(Message&& message);
+            
+        private:
+            /** \brief Sends \a message down to the submodules selected by the supplied index sequence under the premanager, by lvalue reference.
+             *
+             * If this function is passed an lvalue, then this function returns an lvalue reference.
+             * Otherwise, this function returns \a message by move.
+             *
+             * \return `std::forward<Message>(message)`
+             */
+            template< class Message, std::size_t... I >
+            constexpr Message
+            myynt_SendToModules(Message&& message, std::index_sequence<I...>);
+            
+            /** \brief Registers the inheriting manager with the submodule emitters. */
+            template< std::size_t... I >
+            constexpr void
+            myynt_RegisterAsManager(std::index_sequence<I...>) noexcept;
+            
+            /** \brief Registers the inheriting manager with the submodule emitters. */
+            constexpr void
+            myynt_RegisterAsManager() noexcept {
+                myynt_RegisterAsManager(std::make_index_sequence<yymp::size<module_types>::value>());
+            }
+            
+        };
         
-        /** \brief Constructs the premanager's modules from \a modules. 
-         *
-         * This constructor is enabled if and only if all of the following are true:
-         *  * `sizeof...(UTypes) >= 1`
-         *  * `sizeof...(UTypes) == sizeof...(Modules)`
-         *  * `std::is_constructible<complete_with_this_manager<Modules>, U&&>` is \c true for each type `U` in the pack `UTypes`
-         */
-        template<
-            class... UTypes, 
-            class = typename std::enable_if<
-                sizeof...(UTypes) >= 1 &&
-                sizeof...(UTypes) == sizeof...(Modules) &&
-                (std::is_constructible<complete_with_this_manager<Modules>, UTypes&&>::value && ...)    
-            >::type
-        > explicit constexpr premanager(UTypes&&... modules) : modules_(std::forward<UTypes>(modules)...) {
-            myynt_RegisterAsManager();
-        }
-        
-        /** \brief Sends \a message down to the submodules under the premanager, by lvalue reference.
-         *
-         * If this function is passed an lvalue, then this function returns an lvalue reference.
-         * Otherwise, this function this function attempts to return \a message by move.
-         *
-         * \return `std::forward<Message>(message)`
-         */
+        template< class Manager, class... Modules >
         template< class Message >
         constexpr Message
-        myynt_Process(Message&& message);
-        
-    private:
-        /** \brief Sends \a message down to the submodules selected by the supplied index sequence under the premanager, by lvalue reference.
-         *
-         * If this function is passed an lvalue, then this function returns an lvalue reference.
-         * Otherwise, this function returns \a message by move.
-         *
-         * \return `std::forward<Message>(message)`
-         */
+        premanager<Manager, Modules...>::myynt_Process(Message&& message) {
+            using message_value_type = typename std::remove_cv<typename std::remove_reference<Message>::type>::type;
+            
+            using modules = typename yymp::filter<
+                yymp::bind<is_message_processable<message_value_type&, yymp::var>>::template generic,
+                distinct_module_types
+            >::type;
+            
+            // unordered
+            using modules_by_category = typename yymp::group_by<
+                yymp::bind<module_process_category<yymp::var, message_value_type>>::template generic,
+                modules
+            >::type;
+            
+            // ordered
+            using ordered_modules_by_category = typename yymp::transform<
+                yymp::bind<reorder_process_group<message_value_type, yymp::var>>::template generic,
+                modules_by_category
+            >::type; // have fun looking at the implementation of this x.x
+            /* as best i can describe it:
+             * for each group, group each type under the representative tag (tags::tags_of_category filters duplicates for you)
+             * then transform each tag into the group types in this grouping of subgroups (i.e. use the group as a map)
+             * then expand the resulting typelist through join, as it is a typelist of typelists
+             * then wrap it back as a typelist under the category
+             */
+            
+            using first_modules = typename yymp::get_group<tags::first, ordered_modules_by_category>::type;
+            using last_modules = typename yymp::get_group<tags::last, ordered_modules_by_category>::type;
+            using intermediate_modules = typename yymp::difference<
+                modules,
+                typename yymp::join<first_modules, last_modules>::type
+            >::type;
+            
+            using first_indices = typename yymp::indices_within<module_types, first_modules>::type;
+            using last_indices = typename yymp::indices_within<module_types, last_modules>::type;
+            using intermediate_indices = typename yymp::indices_within<module_types, intermediate_modules>::type;
+            
+            using indices = typename yymp::typelist_to_integer_sequence<
+                typename yymp::expand<
+                    yymp::join,
+                    yymp::typelist<
+                        typename yymp::integer_sequence_to_typelist<first_indices>::type,
+                        typename yymp::integer_sequence_to_typelist<intermediate_indices>::type,
+                        typename yymp::integer_sequence_to_typelist<last_indices>::type
+                    >
+                >::type,
+                std::size_t
+            >::type;
+            
+            return myynt_SendToModules(std::forward<Message>(message), indices{});
+        }
+            
+        template< class Manager, class... Modules >
         template< class Message, std::size_t... I >
         constexpr Message
-        myynt_SendToModules(Message&& message, std::index_sequence<I...>);
-        
-        /** \brief Registers the inheriting manager with the submodule emitters. */
-        template< std::size_t... I >
-        constexpr void
-        myynt_RegisterAsManager(std::index_sequence<I...>) noexcept;
-        
-        /** \brief Registers the inheriting manager with the submodule emitters. */
-        constexpr void
-        myynt_RegisterAsManager() noexcept {
-            myynt_RegisterAsManager(std::make_index_sequence<yymp::size<module_types>::value>());
+        premanager<Manager, Modules...>::myynt_SendToModules(Message&& message, std::index_sequence<I...>) {
+            (..., std::get<I>(modules_).myynt_Process(message));
+            return std::forward<Message>(message);
         }
         
-    };
-    
-    template< class Manager, class... Modules >
-    template< class Message >
-    constexpr Message
-    premanager<Manager, Modules...>::myynt_Process(Message&& message) {
-        using message_value_type = typename std::remove_cv<typename std::remove_reference<Message>::type>::type;
-        
-        using modules = typename yymp::filter<
-            yymp::bind<is_message_processable<message_value_type&, yymp::var>>::template generic,
-            distinct_module_types
-        >::type;
-        
-        // unordered
-        using modules_by_category = typename yymp::group_by<
-            yymp::bind<module_process_category<yymp::var, message_value_type>>::template generic,
-            modules
-        >::type;
-        
-        // ordered
-        using ordered_modules_by_category = typename yymp::transform<
-            yymp::bind<reorder_process_group<message_value_type, yymp::var>>::template generic,
-            modules_by_category
-        >::type; // have fun looking at the implementation of this x.x
-        /* as best i can describe it:
-         * for each group, group each type under the representative tag (tags::tags_of_category filters duplicates for you)
-         * then transform each tag into the group types in this grouping of subgroups (i.e. use the group as a map)
-         * then expand the resulting typelist through join, as it is a typelist of typelists
-         * then wrap it back as a typelist under the category
-         */
-        
-        using first_modules = typename yymp::get_group<tags::first, ordered_modules_by_category>::type;
-        using last_modules = typename yymp::get_group<tags::last, ordered_modules_by_category>::type;
-        using intermediate_modules = typename yymp::difference<
-            modules,
-            typename yymp::join<first_modules, last_modules>::type
-        >::type;
-        
-        using first_indices = typename yymp::indices_within<module_types, first_modules>::type;
-        using last_indices = typename yymp::indices_within<module_types, last_modules>::type;
-        using intermediate_indices = typename yymp::indices_within<module_types, intermediate_modules>::type;
-        
-        using indices = typename yymp::typelist_to_integer_sequence<
-            typename yymp::expand<
-                yymp::join,
-                yymp::typelist<
-                    typename yymp::integer_sequence_to_typelist<first_indices>::type,
-                    typename yymp::integer_sequence_to_typelist<intermediate_indices>::type,
-                    typename yymp::integer_sequence_to_typelist<last_indices>::type
-                >
-            >::type,
-            std::size_t
-        >::type;
-        
-        return myynt_SendToModules(std::forward<Message>(message), indices{});
-    }
-        
-    template< class Manager, class... Modules >
-    template< class Message, std::size_t... I >
-    constexpr Message
-    premanager<Manager, Modules...>::myynt_SendToModules(Message&& message, std::index_sequence<I...>) {
-        (..., std::get<I>(modules_).myynt_Process(message));
-        return std::forward<Message>(message);
-    }
-    
-    template< class Manager, class... Modules >
-    template< std::size_t... I >
-    constexpr void
-    premanager<Manager, Modules...>::myynt_RegisterAsManager(std::index_sequence<I...>) noexcept {
-        Manager& manager = *static_cast<Manager*>(this);
-        (..., (myynt_RegisterManagerWithEmitter(manager, std::get<I>(modules_)), void()));
+        template< class Manager, class... Modules >
+        template< std::size_t... I >
+        constexpr void
+        premanager<Manager, Modules...>::myynt_RegisterAsManager(std::index_sequence<I...>) noexcept {
+            Manager& manager = *static_cast<Manager*>(this);
+            (..., (myynt_RegisterManagerWithEmitter(manager, std::get<I>(modules_)), void()));
+        }
     }
 }
 
